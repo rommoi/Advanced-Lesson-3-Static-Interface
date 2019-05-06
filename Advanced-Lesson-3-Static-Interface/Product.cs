59e4cd0caa7178c95ea252e071d21eeb697b4d9d
rommoi <raman.maiseichuk@gmail.com>
2019-05-07 00:02:39 +0300

    Add project files.

---
+++ Advanced-Lesson-3-Static-Interface/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Lesson_3_Static_Interface
+{
+    public class Product
+    {
+        static int ID { get; set; }
+        public int ProductId { get; private set; }
+        public string Name { get; set; }
+
+        static Product()
+        {
+            ID = new Random().Next(1000);
+        }
+
+        public Product(string name)
+        {
+            ProductId = ID++;
+            
+            Name = name;
+        }
+
+        
+
+    }
+}
